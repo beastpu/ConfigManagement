2c1dc1abd0c7cbe5a996c3e7d01d5920c62307ff
piaoge <piao100101@gmail.com>
2020-10-16 14:59:12 +0800

    Update default.py

--- conf/default.py
+++ conf/default.py
@@ -33,7 +33,7 @@ C_FORCE_ROOT = '/root/cmdb/home_applications'
 APP_ID = 'cmdb'
 APP_TOKEN = '0c3ca053-8679-4221-8878-673e633c5252'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://paas.bk.91act.com'
+BK_PAAS_HOST = 'http://paas.bk.**'
 
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
